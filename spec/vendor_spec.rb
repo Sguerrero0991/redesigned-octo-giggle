525f453c0359f5fba8e3e8ff2930dc474795d549
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-03-05 10:29:32 -0600

    Will add attributes to tests

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -6,7 +6,13 @@ RSpec.describe Vendor do
   describe '#initialize' do
     it 'is a Vendor' do
       vendor = Vendor.new("Rocky Mountain Fresh")
-      expect(vednor).to be_a(Vendor)
+      expect(vendor).to be_a(Vendor)
+    end
+
+    it 'has attributes' do
+      vendor = Vendor.new("Rocky Mountain Fresh")
+      expect(vendor.name).to eq("Rocky Mountain Fresh")
+      expect(vendor.inventory).to eq({})
     end
   end
 end
